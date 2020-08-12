61f4ccc15f30f4c4f9104262d27653933e856b33
Moelee <san.limeng@qq.com>
2020-08-12 17:48:57 +0800

    uploade

--- js/index.js
+++ js/index.js
@@ -1,33 +1,101 @@
+var config = {
+    base_link_path: 'https://github.com/EDATRIBE/EDAC/tree/master/',
+};
+var title_href_config = {
+    num_map: {
+        Synchronized: 1,
+        Completed: 2,
+        Incompleted: 3,
+    }
+};
+
 // 定义初值为All
-var edac_nav_tab = 'Synchronized'
 $(".edac_tab").click(function () {
     $.ajaxSettings.async = false;
+    forgetKeyWord();
     var _this = $(this);
-    edac_nav_tab = _this.data('key')
-    render_json_data()
-})
+    var key = _this.data('key');
+    $("#nav_tab").data('key', key);
+    render_json_data();
+    // forgetKeyWord();
+});
 
-function render_json_data(keyword = '') {
+function render_json_data() {
     $.ajaxSettings.async = false;
+    var keyword = $('#key-word').data('value');
     $.getJSON("json/data.json", function (data) {
-        var dom = ''
-        $.each(data[edac_nav_tab], function (index, item) {
-            if (edac_nav_tab == 'Synchronized') { var num = '1' }
-            else if (edac_nav_tab == 'Completed') { var num = '2' }
-            else if (edac_nav_tab == 'Incompleted') { var num = '3' }
-            dom += '<tr><td><a href="https://github.com/EDATRIBE/EDAC/tree/master/' + num + '%20-%20' + edac_nav_tab + '/' + item.link + '">' + item.title_en + '</a>' +
-                '</td><td><a href="https://github.com/EDATRIBE/EDAC/tree/master/' + num + '%20-%20' + edac_nav_tab + '/' + item.link + '">' + item.title_ro + '</a>' +
-                '</td><td>' + item.type +
-                '</td><td>' + item.episodes +
-                '</td><td><a href="https://anidb.net/anime/' + item.anidb_no + '">link</a>' +
-                '</td></tr>'
-        })
-        dom = '<tr><th>English Title</th><th>Romaji Title</th><th>Type</th><th>Ep</th><th>DB</th></tr>' + dom
-        blank = ''
+        var items = formatData(data);
+        var dom = getTableDom(items, keyword);
         $('#catalog').html(dom)
     })
 }
+
+function formatData(data) {
+    var nav_tab_key = $("#nav_tab").data('key');
+    var arr = [];
+    if (nav_tab_key === 'All') {
+        for (var key in data) {
+            var items = data[key];
+            arr.push.apply(arr, items);
+        }
+    } else {
+        arr = data[nav_tab_key];
+    }
+
+    return arr;
+}
+
+function getTableDom(items = [], keyword = '') {
+    var dom = '';
+    $.each(items, function (index, item) {
+        dom += getItemDom(item, keyword);
+    });
+    return dom;
+}
+
+function getItemDom(item, keyword = '') {
+    var title_en = item.title_en;
+    var title_ro = item.title_ro;
+    if (keyword.length !== 0) {
+        var pattern = new RegExp(keyword, 'gmi');
+        if (!pattern.test(title_en + title_ro)) {
+            return '';
+        }
+        //高亮处理
+        title_en = title_en.replace(pattern, function (match) {
+            // console.log(match,param,offset,string);
+            return '<code>' + match + '</code>';
+        });
+        title_ro = title_ro.replace(pattern, function (match) {
+            // console.log(match,param,offset,string);
+            return '<code>' + match + '</code>';
+        });
+        // // 生成正则表达式
+        // var keywordLower = keyword.toLowerCase();
+        // var patternLower = new RegExp(keywordLower, 'gm');
+        // //高亮处理
+        // title_en = title_en.replace(patternLower, '<code style="color:#0f0;background-color:#ff0">' + keywordLower + '</code>');
+        // title_ro = title_ro.replace(patternLower, '<code style="color:#0f0;background-color:#ff0">' + keywordLower + '</code>');
+
+        // var keywordUp = keyword.toUpperCase();
+        // var patternUp = new RegExp(keywordUp, 'gm');
+        // //高亮处理
+        // title_en = title_en.replace(patternUp, '<code style="color:#0f0;background-color:#ff0">' + keywordUp + '</code>');
+        // title_ro = title_ro.replace(patternUp, '<code style="color:#0f0;background-color:#ff0">' + keywordUp + '</code>');
+
+
+    }
+    var title_href = config.base_link_path + title_href_config.num_map[item.category] + '%20-%20' + item.category + '/' + item.link;
+    var dom = '<tr><td><a href="' + title_href + '">' + title_en + '</a>' +
+        '</td><td><a href="' + title_href + '">' + title_ro + '</a>' +
+        '</td><td>' + item.type +
+        '</td><td>' + item.episodes +
+        '</td><td><a href="https://anidb.net/anime/' + item.anidb_no + '">link</a>' +
+        '</td></tr>';
+    return dom;
+}
+
 $(function () {
     render_json_data()
-})
+});
 
--- js/search.js
+++ js/search.js
@@ -1,9 +1,19 @@
 $('#search-button').click(function () {
-    render_json_data($('#key-word').val());
+    searchKeyword();
 });
 
-$('#key-word').keydown(function(event){
-    if (event.keyCode==13){
-        render_json_data($('#key-word').val());
+$('#key-word').keydown(function (event) {
+    if (event.keyCode == 13) {
+        searchKeyword();
     }
-})
+});
+
+function searchKeyword() {
+    $("#key-word").data('value', $('#key-word').val());
+    render_json_data();
+}
+
+function forgetKeyWord() {
+    $('#key-word').val('')
+    $("#key-word").data('value', '');
+}
