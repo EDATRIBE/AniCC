c1d526fac3e6f96cd00468d8ce4a3a955e4403b1
Moelee <san.limeng@qq.com>
2020-08-12 13:16:20 +0800

    upload

--- js/index.js
+++ js/index.js
@@ -1,53 +1,33 @@
+// 定义初值为All
+var edac_nav_tab = 'Synchronized'
+$(".edac_tab").click(function () {
+    $.ajaxSettings.async = false;
+    var _this = $(this);
+    edac_nav_tab = _this.data('key')
+    render_json_data()
+})
+
 function render_json_data(keyword = '') {
     $.ajaxSettings.async = false;
     $.getJSON("json/data.json", function (data) {
         var dom = ''
-        var content = ''
-        $.each(data.Synchronized, function (index, item) {
-            content += '<tr><td>'+item.name_en+
-                    '</td><td>'+item.name_ro+
-                    '</td><td>'+item.type+
-                    '</td><td>'+item.episodes+
-                    '</td><td><a href="https://anidb.net/anime/'+item.anidb_no+'">link</a>'+
-                    '</td></tr>'
-        });
-        dom = '<table border="1"><tr><th>English Name</th><th>Romaji Name</th><th>Type</th><th>Episodes</th><th>Anidb</th></tr>'+content+'</table>'
-        $('.mulu_list').html(dom);
-        console.log(dom)
-    });
+        $.each(data[edac_nav_tab], function (index, item) {
+            if (edac_nav_tab == 'Synchronized') { var num = '1' }
+            else if (edac_nav_tab == 'Completed') { var num = '2' }
+            else if (edac_nav_tab == 'Incompleted') { var num = '3' }
+            dom += '<tr><td><a href="https://github.com/EDATRIBE/EDAC/tree/master/' + num + '%20-%20' + edac_nav_tab + '/' + item.link + '">' + item.title_en + '</a>' +
+                '</td><td><a href="https://github.com/EDATRIBE/EDAC/tree/master/' + num + '%20-%20' + edac_nav_tab + '/' + item.link + '">' + item.title_ro + '</a>' +
+                '</td><td>' + item.type +
+                '</td><td>' + item.episodes +
+                '</td><td><a href="https://anidb.net/anime/' + item.anidb_no + '">link</a>' +
+                '</td></tr>'
+        })
+        dom = '<tr><th>English Title</th><th>Romaji Title</th><th>Type</th><th>Ep</th><th>DB</th></tr>' + dom
+        blank = ''
+        $('#catalog').html(dom)
+    })
 }
+$(function () {
+    render_json_data()
+})
 
-// function getItem(item, keyword = '') {
-//     var number = item.number;
-//     var title = item.title;
-//     if (keyword.length !== 0) {
-//         var pattern = new RegExp(keyword, 'gmi');
-//         if(!pattern.test(number+title)){
-//             return '';
-//         }
-//         // 生成正则表达式
-//         var keywordLower = keyword.toLowerCase();
-//         var patternLower = new RegExp(keywordLower, 'gm');
-//         //高亮处理
-//         number = number.replace(patternLower, '<label style="color:#0f0;background-color:#ff0">' + keywordLower + '</label>');
-//         title = title.replace(patternLower, '<label style="color:#0f0;background-color:#ff0">' + keywordLower + '</label>');
-
-//         var keywordUp = keyword.toUpperCase();
-//         var patternUp = new RegExp(keywordUp, 'gm');
-//         //高亮处理
-//         number = number.replace(patternUp, '<label style="color:#0f0;background-color:#ff0">' + keywordUp + '</label>');
-//         title = title.replace(patternUp, '<label style="color:#0f0;background-color:#ff0">' + keywordUp + '</label>');
-
-
-//     }
-//     return '<li><font face="Microsoft Yahei">[' + number + '] ' + title + '</font></li>';
-// }
-
-// function init()
-// {
-//     render_json_data();
-// }
-
-// $(function () {
-//     init();
-// });
